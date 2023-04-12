8410fc602a0f9794f7ac18414402a45752542036
rudrabhikadiya3 <rudra.rnw@gmail.com>
2023-04-12 17:07:32 +0530

    11/07

--- lib/Screens/HomeScreen/view/HomePage.dart
+++ lib/Screens/HomeScreen/view/HomePage.dart
@@ -16,24 +16,85 @@ class _HomePageState extends State<HomePage> {
       mainAxisAlignment: MainAxisAlignment.start,
       crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Padding(
-          padding: EdgeInsets.only(top: Get.width/60),
-          child: Text(
-            "Events / Announcement",
-            style: TextStyle(
-              color: Colors.white,
-              fontWeight: FontWeight.w500,
-              fontSize: 15.sp
-            ),
-          ),
-        ),
+        // Padding(
+        //   padding: EdgeInsets.only(top: Get.width/60),
+        //   child: Text(
+        //     "Events / Announcement",
+        //     style: TextStyle(
+        //       color: Colors.white,
+        //       fontWeight: FontWeight.w500,
+        //       fontSize: 15.sp
+        //     ),
+        //   ),
+        // ),
+        // Container(
+        //   height: Get.height/4.2,
+        //   width: Get.width/2.4,
+        //   margin: EdgeInsets.only(top: Get.width/60),
+        //   decoration: BoxDecoration(
+        //     color: Colors.red,
+        //     borderRadius: BorderRadius.circular(9)
+        //   ),
+        // )
         Container(
-          height: Get.height/4.2,
-          width: Get.width/2.4,
-          margin: EdgeInsets.only(top: Get.width/60),
+          margin: EdgeInsets.only(top: Get.width/40),
+          height: Get.height/9,
+          width: Get.width/1.3,
           decoration: BoxDecoration(
-            color: Colors.red,
-            borderRadius: BorderRadius.circular(9)
+            gradient: LinearGradient(
+              begin: Alignment.topCenter,
+              end: Alignment.bottomCenter,
+              colors: [
+                Color(0xFF8E58EF),
+                Color(0xFF537AF3),
+              ]
+            ),
+            borderRadius: BorderRadius.circular(15)
+          ),
+          padding: EdgeInsets.only(left: Get.width/40,top: Get.width/25),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Text(
+                "100% BONUS AND IC ON CASH",
+                style: TextStyle(
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold,
+                  fontSize: 12.sp
+                ),
+              ),
+              Padding(
+                padding: EdgeInsets.only(top: Get.width/60),
+                child: Text(
+                  "GET UPTO BONUS AND T20 IC ON FIRST ADD CASH",
+                  style: TextStyle(
+                    color: Color(0xFFFFE400),
+                    fontWeight: FontWeight.bold,
+                    fontSize: 7.sp
+                  ),
+                ),
+              ),
+              Container
+                (
+                height: Get.height/30,
+                width: Get.width/4.5,
+                margin: EdgeInsets.only(top: Get.width/80),
+                decoration: BoxDecoration(
+                  color: Color(0xFFFFE400),
+                  borderRadius: BorderRadius.circular(6)
+                ),
+                alignment: Alignment.center,
+                child: Text(
+                  "Claim Coins",
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 10.sp,
+                    fontWeight: FontWeight.bold
+                  ),
+                ),
+              )
+            ],
           ),
         )
       ],
--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:cricstreak/Screens/navigator/view/navigatorPage.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 import 'package:sizer/sizer.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  SystemChrome.setPreferredOrientations([
+    DeviceOrientation.portraitUp
+  ]);
   runApp(
     Sizer(builder: (context, orientation, deviceType) => GetMaterialApp(
       debugShowCheckedModeBanner: false,
