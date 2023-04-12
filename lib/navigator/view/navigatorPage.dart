f2b4692044b3de5b6e75805533b8e98ecb151bfa
harshramavat01 <harshramavat01@gmail.com>
2023-04-12 10:47:32 +0530

    1247

--- lib/navigator/view/navigatorPage.dart
+++ lib/navigator/view/navigatorPage.dart
@@ -9,13 +9,15 @@ class NavigatorPage extends StatefulWidget {
 }
 
 class _NavigatorPageState extends State<NavigatorPage> {
-  int i =0;
+  int i = 0;
+
   @override
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
         appBar: AppBar(
-          backgroundColor: Color(0xE6021852),
+          leading: CircleAvatar(backgroundImage: NetworkImage(""),radius: 25,),
+          backgroundColor: Color(0xff021852),
         ),
         body: Container(
           height: double.infinity,
@@ -26,54 +28,53 @@ class _NavigatorPageState extends State<NavigatorPage> {
               end: Alignment.bottomCenter,
               colors: [
                 Color(0xD9021852),
-                Color(0xbf021852),
+                Color(0xb3021852),
                 Color(0xd9021852),
               ],
             ),
           ),
         ),
-        bottomNavigationBar: BottomNavigationBar(
-          showSelectedLabels: false,currentIndex: i,
+
+        bottomNavigationBar: BottomNavigationBar(type: BottomNavigationBarType.fixed,
+          showSelectedLabels: false,
+          currentIndex: i,
           showUnselectedLabels: false,
           onTap: (value) {
             setState(() {
-              i=value;
+              i = value;
             });
           },
           backgroundColor: Color(0xff021852),
           items: [
             BottomNavigationBarItem(
-                activeIcon:
-                    Image.asset("assets/image/activehome.png", height: 25),
-                backgroundColor: Color(0xff021852),
-                icon: Image.asset("assets/image/home.png", height: 25),
-                label: ""),
+              activeIcon:
+                  Image.asset("assets/image/activehome.png", height: 25),
+              backgroundColor: Color(0xff021852),
+              icon: Image.asset("assets/image/home.png", height: 25),
+              label: "",
+            ),
             BottomNavigationBarItem(
                 backgroundColor: Color(0xff021852),
-
                 activeIcon:
-                Image.asset("assets/image/activecricket.png", height: 25),
+                    Image.asset("assets/image/activecricket.png", height: 25),
                 icon: Image.asset("assets/image/cricket.png", height: 25),
                 label: ""),
             BottomNavigationBarItem(
                 backgroundColor: Color(0xff021852),
-
                 activeIcon:
-                Image.asset("assets/image/activecard.png", height: 25),
+                    Image.asset("assets/image/activecard.png", height: 25),
                 icon: Image.asset("assets/image/card.png", height: 25),
                 label: ""),
             BottomNavigationBarItem(
                 backgroundColor: Color(0xff021852),
-
                 activeIcon:
-                Image.asset("assets/image/activewallet.png", height: 25),
+                    Image.asset("assets/image/activewallet.png", height: 25),
                 icon: Image.asset("assets/image/wallet.png", height: 25),
                 label: ""),
             BottomNavigationBarItem(
                 backgroundColor: Color(0xff021852),
-
                 activeIcon:
-                Image.asset("assets/image/activegames.png", height: 25),
+                    Image.asset("assets/image/activegames.png", height: 25),
                 icon: Image.asset("assets/image/game.png", height: 25),
                 label: ""),
           ],
