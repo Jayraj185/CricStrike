df48d513fc7502a20416d9bd1ba26238c508e5a1
Drashti <drashti.vaghasiya177@gmail.com>
2023-05-03 16:03:07 +0530

    3/5 4:02

--- lib/Screens/game/Controller/GameController.dart
+++ lib/Screens/game/Controller/GameController.dart
@@ -4,7 +4,7 @@ import 'package:get/get.dart';
 class GameController extends GetxController
 {
   RxList<GameModel> GameList = <GameModel>[
-    GameModel(name: "The Impossible Quizmas", link: "https://poki.com/en/g/the-impossible-quiz", playres: "215", image: "https://img.poki.com/cdn-cgi/image/quality=78,width=94,height=94,fit=cover,f=auto/1f039a07-7c20-4f04-8885-992c45a87f65.jpg    "),
+    GameModel(name: "The Impossible Quizmas", link: "https://poki.com/en/g/the-impossible-quiz", playres: "215", image: "https://img.poki.com/cdn-cgi/image/quality=78,width=94,height=94,fit=cover,f=auto/1f039a07-7c20-4f04-8885-992c45a87f65.jpg"),
     GameModel(name: "Super Star Car", link: "https://poki.com/en/g/super-star-car", playres: "512", image: "https://img.poki.com/cdn-cgi/image/quality=78,width=94,height=94,fit=cover,f=auto/8376e3a632b640cd3e9ca53b9963000a.jpeg"),
     GameModel(name: "Penalty Shooters 2", link: "https://poki.com/en/g/penalty-shooters-2", playres: "300", image: "https://img.poki.com/cdn-cgi/image/quality=78,width=94,height=94,fit=cover,f=auto/7b6f54cf-e1b2-4e69-a84e-1c1f49e54c53.png"),
     GameModel(name: "Cricket World Cup", link: "https://poki.com/en/g/cricket-world-cup", playres: "1085", image: "https://img.poki.com/cdn-cgi/image/quality=78,width=94,height=94,fit=cover,f=auto/a9d84b22bc87dc97bcd8ca7a60893404.png"),
--- lib/Screens/game/view/gamepage.dart
+++ lib/Screens/game/view/gamepage.dart
@@ -1,4 +1,8 @@
+import 'package:cricstreak/Screens/game/Controller/GameController.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class GamePage extends StatefulWidget {
   const GamePage({Key? key}) : super(key: key);
@@ -8,10 +12,136 @@ class GamePage extends StatefulWidget {
 }
 
 class _GamePageState extends State<GamePage> {
+  GameController gameController = Get.put(GameController());
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [],
+      mainAxisAlignment: MainAxisAlignment.start,
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        const Padding(
+          padding: EdgeInsets.only(top: 9),
+          child: Text(
+            "Events / Announcements",
+            style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+              fontSize: 18
+            ),
+          ),
+        ),
+        Padding(
+          padding: const EdgeInsets.only(top: 9,),
+          child: SingleChildScrollView(
+            scrollDirection: Axis.horizontal,
+            physics: BouncingScrollPhysics(),
+            child: Row(
+              children: [
+                Container(
+                  height: 215,
+                  width: 170,
+                  child: Image.asset("assets/image/Events_1.png",fit: BoxFit.fill,),
+                ),
+                SizedBox(width: 9,),
+                Container(
+                  height: 215,
+                  width: 170,
+                  child: Image.asset("assets/image/Events_2.png",fit: BoxFit.fill,),
+                ),
+                SizedBox(width: 9,),
+                Container(
+                  height: 215,
+                  width: 170,
+                  child: Image.asset("assets/image/Events_3.png",fit: BoxFit.fill,),
+                ),
+              ],
+            ),
+          ),
+        ),
+        SizedBox(height: 9,),
+        Divider(thickness: 2,color: Colors.grey,),
+        SizedBox(height: 9,),
+        const Text(
+          "Games",
+          style: TextStyle(
+              color: Colors.white,
+              fontWeight: FontWeight.w400,
+              fontSize: 18
+          ),
+        ),
+        SizedBox(height: 9,),
+        Expanded(
+          child: GridView.builder(
+            physics: const BouncingScrollPhysics(),
+            itemCount: gameController.GameList.length,
+              gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2,crossAxisSpacing: 9,mainAxisExtent: 185,mainAxisSpacing: 9),
+              itemBuilder: (context, index) {
+                return InkWell(
+                  onTap: () async {
+                    await launchUrl(Uri.parse("${gameController.GameList[index].link}"));
+                  },
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.red,
+                      borderRadius: BorderRadius.circular(20)
+                    ),
+                    child: Stack(
+                      children: [
+                        Align(alignment: Alignment.center,child: ClipRRect(borderRadius: BorderRadius.circular(20),child: Image.network("${gameController.GameList[index].image}",fit: BoxFit.fill,height: double.infinity,width: double.infinity,))),
+                        Align(
+                          alignment: Alignment.bottomCenter,
+                          child: Container(
+                            height: 55,
+                            width: double.infinity,
+                            decoration: BoxDecoration(
+                              borderRadius: BorderRadius.only(bottomLeft: Radius.circular(20),bottomRight: Radius.circular(20)),
+                              color: Colors.black26
+                            ),
+                            padding: EdgeInsets.symmetric(horizontal: 15,vertical: 9),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "${gameController.GameList[index].name}",
+                                  maxLines: 1,
+                                  overflow: TextOverflow.ellipsis,
+                                  style: const TextStyle(
+                                    color: Colors.white,
+                                    fontWeight: FontWeight.bold,
+                                    fontSize: 14,
+                                  ),
+                                ),
+                                SizedBox(height: 6,),
+                                Row(
+                                  children: [
+                                    Container(
+                                      height: 12,
+                                      width: 12,
+                                      child: Image.asset("assets/image/players_logo.png"),
+                                    ),
+                                    SizedBox(width: 6,),
+                                    Text(
+                                      "${gameController.GameList[index].playres} players",
+                                      style: const TextStyle(
+                                        color: Colors.white,
+                                        fontSize: 12,
+                                      ),
+                                    ),
+                                  ],
+                                ),
+                              ],
+                            ),
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                );
+              },
+          ),
+        )
+      ],
     );
   }
 }
