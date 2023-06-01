c0f6914382a17b161f8d5ac48aba6ff5b38e39d3
tushar radadiya <tradadiya111>
2023-06-01 21:00:15 +0530

    tushar 20:59 01/06

--- lib/Screens/sign_in/view/signInPage.dart
+++ lib/Screens/sign_in/view/signInPage.dart
@@ -2,9 +2,11 @@
 
 import 'package:country_code_picker/country_code_picker.dart';
 import 'package:cricstreak/Utils/firehelper.dart';
+import 'package:edge_alerts/edge_alerts.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:get/get.dart';
 
 class SignInPage extends StatefulWidget {
@@ -22,184 +24,200 @@ class _SignInPageState extends State<SignInPage> {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Container(
-          height: double.infinity,
-          width: double.infinity,
-          decoration: const BoxDecoration(
-            gradient: LinearGradient(
-              begin: Alignment.topCenter,
-              end: Alignment.bottomCenter,
-              colors: [
-                Color(0xD9021852),
-                Color(0xb3021852),
-                Color(0xd9021852),
-              ],
+        body: Stack(
+          children: [
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                image: DecorationImage(
+                    image: NetworkImage(
+                        "https://i.pinimg.com/564x/3e/cc/0a/3ecc0a5051ceee48689a480a00d62dac.jpg"),
+                    fit: BoxFit.fill),
+              ),
             ),
-          ),
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: SingleChildScrollView(
-              physics: const BouncingScrollPhysics(),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: [
-                  const Text(
-                    "SIGN IN",
-                    style: TextStyle(
-                      color: Colors.white,
-                      fontSize: 18,
-                      fontWeight: FontWeight.bold,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 80,
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                        color: Colors.white,
-                        borderRadius: BorderRadius.circular(7)),
-                    child: TextField(
-                      keyboardType: TextInputType.phone,
-                      cursorColor: Colors.black,
-                      decoration: InputDecoration(
-                        prefixIcon: Row(
-                          mainAxisSize: MainAxisSize.min,
-                          children: [
-                            Stack(
+            Container(
+              height: double.infinity,
+              width: double.infinity,
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Color(0xD9021852),
+                    Color(0xb3021852),
+                    Color(0xd9021852),
+                  ],
+                ),
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(16.0),
+                child: SingleChildScrollView(
+                  physics: const BouncingScrollPhysics(),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      const Text(
+                        "SIGN IN",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 18,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                      const SizedBox(
+                        height: 80,
+                      ),
+                      Container(
+                        decoration: BoxDecoration(
+                            color: Colors.white,
+                            borderRadius: BorderRadius.circular(7)),
+                        child: TextField(
+                          keyboardType: TextInputType.phone,
+                          cursorColor: Colors.black,
+                          decoration: InputDecoration(
+                            prefixIcon: Row(
+                              mainAxisSize: MainAxisSize.min,
                               children: [
-                                CountryCodePicker(
-                                  showDropDownButton: true,
-                                  showFlagMain: true,
-                                  padding: const EdgeInsets.only(right: 0, left: 0),
+                                Stack(
+                                  children: [
+                                    CountryCodePicker(
+                                      showDropDownButton: true,
+                                      showFlagMain: true,
+                                      padding:
+                                          const EdgeInsets.only(right: 0, left: 0),
 
-                                  onChanged: (value) {
-                                    print(
-                                        "================================================================================================================> ${value!.dialCode}");
-                                    code = value.dialCode!;
-                                    print;
-                                  },
+                                      onChanged: (value) {
+                                        print(
+                                            "================================================================================================================> ${value!.dialCode}");
+                                        code = value.dialCode!;
+                                        print;
+                                      },
 
-                                  // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                                      // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
 
-                                  initialSelection: '+91',
-                                  // builder: (p0) {
-                                  //   print("================> $p0");
-                                  // },
-                                  favorite: ['+91', 'IND'],
+                                      initialSelection: '+91',
+                                      // builder: (p0) {
+                                      //   print("================> $p0");
+                                      // },
+                                      favorite: ['+91', 'IND'],
 
-                                  // optional. Shows only country name and flag
+                                      // optional. Shows only country name and flag
 
-                                  showCountryOnly: false,
-                                  flagDecoration: Decoration.lerp(
-                                      BoxDecoration(
-                                          borderRadius:
-                                              BorderRadius.circular(4)),
-                                      const BoxDecoration(),
-                                      0),
-                                  showFlag: true,
+                                      showCountryOnly: false,
+                                      flagDecoration: Decoration.lerp(
+                                          BoxDecoration(
+                                              borderRadius:
+                                                  BorderRadius.circular(4)),
+                                          const BoxDecoration(),
+                                          0),
+                                      showFlag: true,
 
-                                  // optional. Shows only country name and flag when popup is closed.
+                                      // optional. Shows only country name and flag when popup is closed.
 
-                                  showOnlyCountryWhenClosed: false,
+                                      showOnlyCountryWhenClosed: false,
 
-                                  // optional. aligns the flag and the Text left
+                                      // optional. aligns the flag and the Text left
 
-                                  alignLeft: false,
+                                      alignLeft: false,
 
-                                  boxDecoration:
-                                      const BoxDecoration(color: Colors.white),
-                                ),
-                                // Padding(
-                                //   padding:
-                                //       EdgeInsets.only(left: 80, top: 12),
-                                //   child: InkWell(child: Icon(
-                                //       Icons.arrow_drop_down,
-                                //       color: Colors.black,
-                                //     ) ,
-                                //     onTap: () {return
-                                //
-                                //
-                                //        CountryCodePicker(
-                                //         padding: EdgeInsets.only(right: 0, left: 0),
-                                //
-                                //         onChanged: print,
-                                //
-                                //         // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
-                                //
-                                //         initialSelection: 'IND',
-                                //
-                                //         favorite: ['+91', 'IND'],
-                                //
-                                //         // optional. Shows only country name and flag
-                                //
-                                //         showCountryOnly: true,
-                                //
-                                //         showFlag: true,
-                                //
-                                //         // optional. Shows only country name and flag when popup is closed.
-                                //
-                                //         showOnlyCountryWhenClosed: false,
-                                //
-                                //         // optional. aligns the flag and the Text left
-                                //
-                                //         alignLeft: false,
-                                //
-                                //         boxDecoration:
-                                //         BoxDecoration(color: Colors.white),
-                                //       );
-                                //
-                                //     },
-                                //   ),
-                                // ),
-                                Padding(
-                                  padding: const EdgeInsets.only(
-                                      top: 6.0, left: 115),
-                                  child: Container(
-                                    width: 1.5,
-                                    height: 35,
-                                    color: Colors.black45,
-                                  ),
+                                      boxDecoration:
+                                          const BoxDecoration(color: Colors.white),
+                                    ),
+                                    // Padding(
+                                    //   padding:
+                                    //       EdgeInsets.only(left: 80, top: 12),
+                                    //   child: InkWell(child: Icon(
+                                    //       Icons.arrow_drop_down,
+                                    //       color: Colors.black,
+                                    //     ) ,
+                                    //     onTap: () {return
+                                    //
+                                    //
+                                    //        CountryCodePicker(
+                                    //         padding: EdgeInsets.only(right: 0, left: 0),
+                                    //
+                                    //         onChanged: print,
+                                    //
+                                    //         // Initial selection and favorite can be one of code ('IT') OR dial_code('+39')
+                                    //
+                                    //         initialSelection: 'IND',
+                                    //
+                                    //         favorite: ['+91', 'IND'],
+                                    //
+                                    //         // optional. Shows only country name and flag
+                                    //
+                                    //         showCountryOnly: true,
+                                    //
+                                    //         showFlag: true,
+                                    //
+                                    //         // optional. Shows only country name and flag when popup is closed.
+                                    //
+                                    //         showOnlyCountryWhenClosed: false,
+                                    //
+                                    //         // optional. aligns the flag and the Text left
+                                    //
+                                    //         alignLeft: false,
+                                    //
+                                    //         boxDecoration:
+                                    //         BoxDecoration(color: Colors.white),
+                                    //       );
+                                    //
+                                    //     },
+                                    //   ),
+                                    // ),
+                                    Padding(
+                                      padding: const EdgeInsets.only(
+                                          top: 6.0, left: 115),
+                                      child: Container(
+                                        width: 1.5,
+                                        height: 35,
+                                        color: Colors.black45,
+                                      ),
+                                    ),
+                                  ],
                                 ),
                               ],
                             ),
-                          ],
-                        ),
-                        border: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
-                        ),
-                        labelStyle: const TextStyle(color: Colors.black),
-                        hintText: "Enter Mobile Number",
-                        focusedBorder: const OutlineInputBorder(
-                          borderSide: BorderSide(color: Colors.black),
+                            border: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                            labelStyle: const TextStyle(color: Colors.black),
+                            hintText: "Enter Mobile Number",
+                            focusedBorder: const OutlineInputBorder(
+                              borderSide: BorderSide(color: Colors.black),
+                            ),
+                          ),
+                          controller: txtNumber,
                         ),
                       ),
-                      controller: txtNumber,
-                    ),
-                  ),
-                  const SizedBox(
-                    height: 15,
-                  ),
-                  Center(
-                    child: ElevatedButton(
-                      onPressed: () async {
-                        await FirebaseAuth.instance.verifyPhoneNumber(
-                          phoneNumber: '${code} ${txtNumber.text}',
-                          verificationCompleted:
-                              (PhoneAuthCredential credential) {},
-                          verificationFailed: (FirebaseAuthException e) {},
-                          codeSent: (String verificationId, int? resendToken) {
-                            Get.toNamed("otp");
+                      const SizedBox(
+                        height: 15,
+                      ),
+                      Center(
+                        child: ElevatedButton(
+                          onPressed: () async {
+                            edgeAlert(context, title: 'Title', description: 'Description', gravity: Gravity.top);
+                            await FirebaseAuth.instance.verifyPhoneNumber(
+                              phoneNumber: '${code} ${txtNumber.text}',
+                              verificationCompleted:
+                                  (PhoneAuthCredential credential) {},
+                              verificationFailed: (FirebaseAuthException e) {},
+                              codeSent: (String verificationId, int? resendToken) {
+                                Get.toNamed("otp");
+                              },
+                              codeAutoRetrievalTimeout: (String verificationId) {},
+                            );
                           },
-                          codeAutoRetrievalTimeout: (String verificationId) {},
-                        );
-                      },
-                      child: const Text("Get Otp"),
-                    ),
-                  )
-                ],
+                          child: const Text("Get Otp"),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
               ),
             ),
-          ),
+          ],
         ),
       ),
     );
--- lib/main.dart
+++ lib/main.dart
@@ -26,7 +26,7 @@ void main() async {
   runApp(
     GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: 'nav',
+      initialRoute: '/',
       routes: {
         '/': (context) => const SplashScreen(),
         'sign_in': (context) => const SignInPage(),
